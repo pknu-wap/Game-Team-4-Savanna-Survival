9f38b6cc614c195cc5c7e7f48d2eccf9772bef5a
sasanga7777 <chiheon777@gmail.com>
2026-05-06 17:10:11 +0900

    feat: 게임 Ui 매니저 추가
    StartUiObject, TimeUiObject, SettingUiObject 를 버튼 인식을 통해 온오프하는 기능을 추가하였습니다.

---
+++ Assets/Scripts/GameUi/GameUiManager.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class GameUiManager : MonoBehaviour
+{
+    public GameObject startUI;
+    public GameObject gameUI;
+    public GameObject settingUI;
+
+    void Start()
+    {
+        ShowStartUI();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToggleSettingUI();
+        }
+    }
+
+    public void ShowStartUI()
+    {
+        startUI.SetActive(true);
+        gameUI.SetActive(false);
+        settingUI.SetActive(false);
+
+        Time.timeScale = 0f;
+    }
+
+    public void OnStartButton()
+    {
+        startUI.SetActive(false);
+        gameUI.SetActive(true);
+        settingUI.SetActive(false);
+
+        Time.timeScale = 1f;
+    }
+
+    public void ToggleSettingUI()
+    {
+        bool isSettingOpen = settingUI.activeSelf;
+
+        if (isSettingOpen)
+        {
+            settingUI.SetActive(false);
+            gameUI.SetActive(true);
+
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            settingUI.SetActive(true);
+            gameUI.SetActive(false);
+
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void CloseSettingUI()
+    {
+        settingUI.SetActive(false);
+        gameUI.SetActive(true);
+
+        Time.timeScale = 1f;
+    }
+
+    public void OnEndButton()
+    {
+        Application.Quit();
+    }
+}
