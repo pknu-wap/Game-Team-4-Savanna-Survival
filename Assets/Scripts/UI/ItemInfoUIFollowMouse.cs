fe89008cc81796295dd6fdbb2e6ce9a73ceaaef7
Mobyeol <rhgusdn_1@naver.com>
2026-05-21 16:50:13 +0900

    feat: 장비Info 마우스 추적
    - 장비InfoUI가 마우스를 따라오도록 구현
    - Scene폴더 위치 이동

---
+++ Assets/Scripts/UI/ItemInfoUIFollowMouse.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ItemInfoUIFollowMouse : MonoBehaviour
+{
+    [SerializeField] private Vector2 leftTopOffset = new Vector2(10f, -10f);
+    [SerializeField] private Vector2 rightTopOffset = new Vector2(-10f, -10f);
+    [SerializeField] private float rightEdgeThreshold = 230f;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current == null) return;
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        if (mousePos.x > Screen.width - rightEdgeThreshold)
+        {
+            rectTransform.pivot = new Vector2(1f, 1f);
+            rectTransform.position = mousePos + rightTopOffset;
+        }
+        else
+        {
+        rectTransform.pivot = new Vector2(0f, 1f);
+        rectTransform.position = mousePos + leftTopOffset;
+        }
+    }
+}
